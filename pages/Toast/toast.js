636d4738cc27fdadccec498b494496389706c27c
Divya70 <divyanamdev8795@gmail.com>
2022-04-07 20:42:58 +0530

    Feat: add Grid , Text Utilities and Toast

---
+++ pages/Toast/toast.js
@@ -0,0 +1,13 @@
+function showsuccesToast() {
+  document.getElementById("toast-success").style.display = "flex";
+}
+
+function showerrorToast() {
+  document.getElementById("toast-error").style.display = "flex";
+}
+function closesuccesToast() {
+  document.getElementById("toast-success").style.display = "none";
+}
+function closeerrorToast() {
+  document.getElementById("toast-error").style.display = "none";
+}
